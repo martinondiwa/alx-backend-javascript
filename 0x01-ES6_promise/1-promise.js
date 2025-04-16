a17f14567490f3b44c0ddfb4e1db67d2476cc167
Martin Sure <surekondiwa@gmail.com>
2025-04-16 10:40:39 +0000

    asd

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,12 @@
-function getFullResponseFromAPI(success){
-
-	return new promise ((resolve,reject)=>{
-		if (success){
-			resolve(status: 200, body: 'Success')
+function getFullResponseFromAPI(success) {
+	return new promise((resolve, reject) => {
+		if (success) {
+			resolve({status: 200, body: 'Success'})
 		} else {
-			reject (new error('The fake API is not working currently'));
+			reject(new error('The fake API is not working currently'));
 		}
-})
+	})
 }
-
 
 console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
